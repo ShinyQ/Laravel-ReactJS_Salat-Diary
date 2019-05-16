0dc01836d9f1db4d71b4ac133c55d32b1d026374
Kurniadi Ahmad Wijaya <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2019-05-16 14:37:08 +0700

    ADD | CRUD Data Salat

--- server/routes/api.php
+++ server/routes/api.php
@@ -12,16 +12,13 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::group(['middleware' => 'cors'], function () {
-  
+
 Route::post('/login', 'API\V1\Auth\LoginController@login');
 Route::post('/register', 'API\V1\Auth\RegisterController@register');
 Route::get('/logout', 'API\V1\Auth\LoginController@logout');
 
-  Route::group(['middleware' => 'auth:api'], function () {
-      Route::prefix('v1')->group(function(){
-        Route::apiResource("salat", "API\V1\SalatController");
-      });
-  });
-
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::prefix('v1')->group(function(){
+      Route::apiResource("salat", "API\V1\SalatController");
+    });
 });
