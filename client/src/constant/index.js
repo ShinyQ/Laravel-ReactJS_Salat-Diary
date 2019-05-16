1fb3d8044ed7f9168c5a004eb58a2d7f6fae4cd2
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2019-05-16 19:04:08 +0700

    Add Success Register Notification

--- client/src/constant/index.js
+++ client/src/constant/index.js
@@ -5,3 +5,5 @@ export const BASE_URL = 'http://localhost:8000';
 export const GET_CURRENT_POSITION = 'GET_CURRENT_POSITION';
 export const ERR_GET_CURRENT_POSITION = 'ERR_GET_CURRENT_POSITION';
 export const GET_LOGIN = 'GET_LOGIN';
+export const GET_REGISTER = 'GET_REGISTER';
+export const RESET_IS_JUST_REGISTER = 'RESET_IS_JUST_REGISTER';
