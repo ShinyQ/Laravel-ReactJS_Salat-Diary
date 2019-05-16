a1c1da67f19eefb92640e73c4321cb9821491668
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2019-05-17 02:16:43 +0700

    Add Dashboard Layout Design

--- client/src/components/Platform/index.js
+++ client/src/components/Platform/index.js
@@ -1,11 +1,35 @@
 import React, {Component} from 'react';
+import {Layout} from 'antd';
+
+import './style.less';
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
+
+const {Content} = Layout;
+
+
 
 class Platform extends Component {
+
+
     render() {
         return (
-            <div>
-
-            </div>
+            <Layout style={{minHeight: '100vh'}}>
+                <Sidebar/>
+                <Layout>
+                    <Navbar/>
+                    <Content
+                        style={{
+                            margin: '24px 16px',
+                            padding: 24,
+                            background: '#fff',
+                            minHeight: 280,
+                        }}
+                    >
+                        Content
+                    </Content>
+                </Layout>
+            </Layout>
         );
     }
 }
