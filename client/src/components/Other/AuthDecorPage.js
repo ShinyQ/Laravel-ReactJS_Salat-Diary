e93949e788ed12fbfd3dd1139a1e79cdf1d8323d
Kurniadi Ahmad Wijaya <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2019-05-20 17:31:40 +0700

    develop/back_end

--- client/src/components/Other/AuthDecorPage.js
+++ client/src/components/Other/AuthDecorPage.js
@@ -5,11 +5,11 @@ const AuthDecorPage = () => {
     return (
         <div className="login-decor vAlignParent">
             <div className="vAlign">
-                {/*<img src="" alt="shalatDiary"/>*/}
+                <img src="../../../assets/images/logo.png" alt="shalatDiary"/>
                 <h1>Welcome to Salat Diary</h1>
-                <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores aspernatur,
+                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores aspernatur,
                     delectus deserunt dignissimos dolore error explicabo fuga impedit laboriosam minima
-                    necessitatibus neque nihil, rem repellat sit vel veniam voluptas.</h4>
+                    necessitatibus neque nihil, rem repellat sit vel veniam voluptas.</h3>
             </div>
         </div>
     );
