5ee442f0c51d15159b87115d5fc299ce23f5dbb6
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2019-05-21 13:58:56 +0700

    Add Post Salat Today (Validation yet to be implemented)

--- client/src/components/Platform/Home/index.js
+++ client/src/components/Platform/Home/index.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {getCurrentUser} from "../../../actions/authAction";
+import {getKeySalat, getKeyStatus} from "../../../actions/salatAction";
 
-import {Col, Row, Skeleton} from 'antd'
+import {Col, Divider, Row, Skeleton} from 'antd'
 
 import './style.less';
 import HeaderHome from "./HeaderHome";
+import InputDataSalat from "./InputDataSalat";
 
 class Home extends Component {
   
@@ -14,6 +16,8 @@ class Home extends Component {
     };
 
     async componentDidMount() {
+        await this.props.getKeySalat();
+        await this.props.getKeyStatus();
         await this.props.getCurrentUser();
         this.setState({
             isLoading: false
@@ -31,17 +35,19 @@ class Home extends Component {
             return (
                 <div>
                     <Row gutter={32}>
-                        <Col md={24} lg={24} xl={12}>
+                        <Col md={24} lg={24} xl={10}>
                             <div className="headerHome">
                                 <div className="container">
                                     <HeaderHome nama={this.props.user.name}/>
                                 </div>
                             </div>
                             <div className="container">
-                                <h2>Bagaimana salat {this.props.salatNow} hari ini?</h2>
+                                <h2>Bagaimana salat <b>{this.props.salatNow}</b> hari ini ?</h2>
+                                <InputDataSalat salatNow={this.props.salatNow}/>
                             </div>
+                            <Divider/>
                         </Col>
-                        <Col md={24} lg={24} xl={12}>
+                        <Col md={24} lg={24} xl={14}>
 
                         </Col>
                     </Row>
@@ -59,4 +65,4 @@ const mapStateToProps = state => {
     }
 };
 
-export default connect(mapStateToProps, {getCurrentUser})(Home);
+export default connect(mapStateToProps, {getCurrentUser, getKeySalat, getKeyStatus})(Home);
--- client/src/reducers/dataSalatReducer.js
+++ client/src/reducers/dataSalatReducer.js
@@ -1,8 +1,10 @@
-import {SET_JADWAL_SALAT, SET_SALAT_NOW} from "../constant";
+import {GET_KEY_SALAT, GET_KEY_STATUS, SET_JADWAL_SALAT, SET_SALAT_NOW} from "../constant";
 
 const INITIAL_STATE = {
     salatNow: null,
-    jadwalSalat: null
+    jadwalSalat: null,
+    keySalat: null,
+    keyStatus: null
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -11,6 +13,10 @@ export default (state = INITIAL_STATE, action) => {
             return {...state, jadwalSalat: action.payload};
         case SET_SALAT_NOW:
             return {...state, salatNow: action.payload};
+        case GET_KEY_SALAT:
+            return {...state, keySalat: action.payload};
+        case GET_KEY_STATUS:
+            return {...state, keyStatus: action.payload};
         default:
             return state
     }
