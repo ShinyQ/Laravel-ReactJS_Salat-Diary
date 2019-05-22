da80b6e520b87281fad86f8ad5ce7a694fd85616
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2019-05-23 02:23:05 +0700

    Add Countdown for next Salat

--- client/src/components/Platform/JadwalShalat/index.js
+++ client/src/components/Platform/JadwalShalat/index.js
@@ -1,19 +1,39 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux'
+import {Col, Row, Typography} from 'antd';
 
 import {getCurrentUser} from "../../../actions/authAction";
+import {countDownNextSalat, getJadwalShalat} from "../../../actions/jadwalShalatAction";
+
+import './style.less';
+
+const {Title} = Typography;
 
 class JadwalShalat extends Component {
 
-    componentDidMount() {
-        this.props.getCurrentUser()
+    async componentDidMount() {
+        const todayDate = new Date();
+
+        this.props.getCurrentUser();
+        await this.props.getJadwalShalat(todayDate.getMonth() + 1);
+        this.props.countDownNextSalat();
     }
 
+
     render() {
-        console.log(this.props.jadwalSalat);
         return (
             <div>
-                Meki
+                <div className="headerJadwal">
+                    <h1>Salat Selanjutnya</h1>
+                    <Row gutter={32}>
+                        <Col span={12}>
+                            <h5>{this.props.nextSalat}</h5>
+                        </Col>
+                        <Col span={12}>
+                            <h4>{this.props.intervalNextSalat}</h4>
+                        </Col>
+                    </Row>
+                </div>
             </div>
         );
     }
@@ -21,8 +41,10 @@ class JadwalShalat extends Component {
 
 const mapStateToProps = state => {
     return {
-        jadwalSalat: state.dataSalat.jadwalSalat
+        jadwalSalat: state.dataSalat.jadwalSalat,
+        nextSalat: state.dataSalat.nextSalat,
+        intervalNextSalat: state.dataSalat.intervalNextSalat
     }
 };
 
-export default connect(mapStateToProps, {getCurrentUser})(JadwalShalat);
+export default connect(mapStateToProps, {getCurrentUser, countDownNextSalat, getJadwalShalat})(JadwalShalat);
--- client/src/reducers/dataSalatReducer.js
+++ client/src/reducers/dataSalatReducer.js
@@ -1,6 +1,8 @@
 import {
+    GET_INTERVAL_NEXT_SALAT,
     GET_KEY_SALAT,
     GET_KEY_STATUS,
+    GET_NEXT_SALAT,
     GET_SELECTED_DATE_SALAT,
     GET_TODAY_SALAT,
     SET_JADWAL_SALAT,
@@ -14,6 +16,8 @@ const INITIAL_STATE = {
     keyStatus: null,
     todaySalat: [],
     selectedDateSalat: [],
+    intervalNextSalat: null,
+    nextSalat: null,
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -30,6 +34,10 @@ export default (state = INITIAL_STATE, action) => {
             return {...state, todaySalat: action.payload};
         case GET_SELECTED_DATE_SALAT:
             return {...state, selectedDateSalat: action.payload};
+        case GET_INTERVAL_NEXT_SALAT:
+            return {...state, intervalNextSalat: action.payload};
+        case GET_NEXT_SALAT:
+            return {...state, nextSalat: action.payload};
         default:
             return state
     }
